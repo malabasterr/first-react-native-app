595079f4c48679c04fc1ef5b3aa5993bb5e3e5e2
malabasterr <113557081+malabasterr@users.noreply.github.com>
2024-03-20 22:55:22 +0000

    add homepage button

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'ArcadeClassic': require('./assets/fonts/ArcadeClassic.ttf'),
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground 
+    source={require('./assets/celesteBg.jpg')} 
+    resizeMode="cover" 
+    style={styles.image}>
+      <Text style={styles.text}>THIS IS MY TEST TEXT</Text>
+      <Button title="Press Me" />
+    </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +22,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  image: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'ArcadeClassic',
+    fontSize: 50,
   },
 });
